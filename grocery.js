f13e5be077248b7912301c1a9e10f30af310685a
Brittnee Boyer <brittneewestra98@gmail.com>
2021-11-15 17:28:41 -0700

    Commit

---
+++ grocery.js
@@ -0,0 +1,11 @@
+var shopper = {
+    store: "Smiths",
+    price: 20,
+    credit: true,
+    groceryCart: ["bread", "cheese", "eggs", "milk"],
+    fullName: function(){
+        return this.store + " Food and Drug"
+    }
+}
+
+console.log(shopper)
